6112d4baf40f805604b79ab2183aa4286bdf1628
CK9797 <76744421+CK9797@users.noreply.github.com>
2021-01-11 15:25:45 +0800

    Add files via upload

---
+++ angular-app/e2e/app.e2e-spec.ts
@@ -0,0 +1,336 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { AngularTestPage } from './app.po';
+import { ExpectedConditions, browser, element, by } from 'protractor';
+import {} from 'jasmine';
+
+
+describe('Starting tests for angular-app', function() {
+  let page: AngularTestPage;
+
+  beforeEach(() => {
+    page = new AngularTestPage();
+  });
+
+  it('website title should be angular-app', () => {
+    page.navigateTo('/');
+    return browser.getTitle().then((result)=>{
+      expect(result).toBe('angular-app');
+    })
+  });
+
+  it('network-name should be crmblockchain@0.0.61',() => {
+    element(by.css('.network-name')).getWebElement()
+    .then((webElement) => {
+      return webElement.getText();
+    })
+    .then((txt) => {
+      expect(txt).toBe('crmblockchain@0.0.61.bna');
+    });
+  });
+
+  it('navbar-brand should be angular-app',() => {
+    element(by.css('.navbar-brand')).getWebElement()
+    .then((webElement) => {
+      return webElement.getText();
+    })
+    .then((txt) => {
+      expect(txt).toBe('angular-app');
+    });
+  });
+
+  
+    it('proposal component should be loadable',() => {
+      page.navigateTo('/proposal');
+      browser.findElement(by.id('assetName'))
+      .then((assetName) => {
+        return assetName.getText();
+      })
+      .then((txt) => {
+        expect(txt).toBe('proposal');
+      });
+    });
+
+    it('proposal table should have 9 columns',() => {
+      page.navigateTo('/proposal');
+      element.all(by.css('.thead-cols th')).then(function(arr) {
+        expect(arr.length).toEqual(9); // Addition of 1 for 'Action' column
+      });
+    });
+  
+    it('contract component should be loadable',() => {
+      page.navigateTo('/contract');
+      browser.findElement(by.id('assetName'))
+      .then((assetName) => {
+        return assetName.getText();
+      })
+      .then((txt) => {
+        expect(txt).toBe('contract');
+      });
+    });
+
+    it('contract table should have 10 columns',() => {
+      page.navigateTo('/contract');
+      element.all(by.css('.thead-cols th')).then(function(arr) {
+        expect(arr.length).toEqual(10); // Addition of 1 for 'Action' column
+      });
+    });
+  
+    it('PO component should be loadable',() => {
+      page.navigateTo('/PO');
+      browser.findElement(by.id('assetName'))
+      .then((assetName) => {
+        return assetName.getText();
+      })
+      .then((txt) => {
+        expect(txt).toBe('PO');
+      });
+    });
+
+    it('PO table should have 7 columns',() => {
+      page.navigateTo('/PO');
+      element.all(by.css('.thead-cols th')).then(function(arr) {
+        expect(arr.length).toEqual(7); // Addition of 1 for 'Action' column
+      });
+    });
+  
+    it('Invoice component should be loadable',() => {
+      page.navigateTo('/Invoice');
+      browser.findElement(by.id('assetName'))
+      .then((assetName) => {
+        return assetName.getText();
+      })
+      .then((txt) => {
+        expect(txt).toBe('Invoice');
+      });
+    });
+
+    it('Invoice table should have 13 columns',() => {
+      page.navigateTo('/Invoice');
+      element.all(by.css('.thead-cols th')).then(function(arr) {
+        expect(arr.length).toEqual(13); // Addition of 1 for 'Action' column
+      });
+    });
+  
+    it('deal component should be loadable',() => {
+      page.navigateTo('/deal');
+      browser.findElement(by.id('assetName'))
+      .then((assetName) => {
+        return assetName.getText();
+      })
+      .then((txt) => {
+        expect(txt).toBe('deal');
+      });
+    });
+
+    it('deal table should have 11 columns',() => {
+      page.navigateTo('/deal');
+      element.all(by.css('.thead-cols th')).then(function(arr) {
+        expect(arr.length).toEqual(11); // Addition of 1 for 'Action' column
+      });
+    });
+  
+    it('SaleOrder component should be loadable',() => {
+      page.navigateTo('/SaleOrder');
+      browser.findElement(by.id('assetName'))
+      .then((assetName) => {
+        return assetName.getText();
+      })
+      .then((txt) => {
+        expect(txt).toBe('SaleOrder');
+      });
+    });
+
+    it('SaleOrder table should have 10 columns',() => {
+      page.navigateTo('/SaleOrder');
+      element.all(by.css('.thead-cols th')).then(function(arr) {
+        expect(arr.length).toEqual(10); // Addition of 1 for 'Action' column
+      });
+    });
+  
+
+  
+    it('Customer component should be loadable',() => {
+      page.navigateTo('/Customer');
+      browser.findElement(by.id('participantName'))
+      .then((participantName) => {
+        return participantName.getText();
+      })
+      .then((txt) => {
+        expect(txt).toBe('Customer');
+      });
+    });
+
+    it('Customer table should have 6 columns',() => {
+      page.navigateTo('/Customer');
+      element.all(by.css('.thead-cols th')).then(function(arr) {
+        expect(arr.length).toEqual(6); // Addition of 1 for 'Action' column
+      });
+    });
+  
+    it('Staff component should be loadable',() => {
+      page.navigateTo('/Staff');
+      browser.findElement(by.id('participantName'))
+      .then((participantName) => {
+        return participantName.getText();
+      })
+      .then((txt) => {
+        expect(txt).toBe('Staff');
+      });
+    });
+
+    it('Staff table should have 5 columns',() => {
+      page.navigateTo('/Staff');
+      element.all(by.css('.thead-cols th')).then(function(arr) {
+        expect(arr.length).toEqual(5); // Addition of 1 for 'Action' column
+      });
+    });
+  
+
+  
+    it('acceptProposal component should be loadable',() => {
+      page.navigateTo('/acceptProposal');
+      browser.findElement(by.id('transactionName'))
+      .then((transactionName) => {
+        return transactionName.getText();
+      })
+      .then((txt) => {
+        expect(txt).toBe('acceptProposal');
+      });
+    });
+  
+    it('rejectProposal component should be loadable',() => {
+      page.navigateTo('/rejectProposal');
+      browser.findElement(by.id('transactionName'))
+      .then((transactionName) => {
+        return transactionName.getText();
+      })
+      .then((txt) => {
+        expect(txt).toBe('rejectProposal');
+      });
+    });
+  
+    it('acceptContract component should be loadable',() => {
+      page.navigateTo('/acceptContract');
+      browser.findElement(by.id('transactionName'))
+      .then((transactionName) => {
+        return transactionName.getText();
+      })
+      .then((txt) => {
+        expect(txt).toBe('acceptContract');
+      });
+    });
+  
+    it('rejectContract component should be loadable',() => {
+      page.navigateTo('/rejectContract');
+      browser.findElement(by.id('transactionName'))
+      .then((transactionName) => {
+        return transactionName.getText();
+      })
+      .then((txt) => {
+        expect(txt).toBe('rejectContract');
+      });
+    });
+  
+    it('initiatePO component should be loadable',() => {
+      page.navigateTo('/initiatePO');
+      browser.findElement(by.id('transactionName'))
+      .then((transactionName) => {
+        return transactionName.getText();
+      })
+      .then((txt) => {
+        expect(txt).toBe('initiatePO');
+      });
+    });
+  
+    it('updatePaymentStatus component should be loadable',() => {
+      page.navigateTo('/updatePaymentStatus');
+      browser.findElement(by.id('transactionName'))
+      .then((transactionName) => {
+        return transactionName.getText();
+      })
+      .then((txt) => {
+        expect(txt).toBe('updatePaymentStatus');
+      });
+    });
+  
+    it('createProposal component should be loadable',() => {
+      page.navigateTo('/createProposal');
+      browser.findElement(by.id('transactionName'))
+      .then((transactionName) => {
+        return transactionName.getText();
+      })
+      .then((txt) => {
+        expect(txt).toBe('createProposal');
+      });
+    });
+  
+    it('createContract component should be loadable',() => {
+      page.navigateTo('/createContract');
+      browser.findElement(by.id('transactionName'))
+      .then((transactionName) => {
+        return transactionName.getText();
+      })
+      .then((txt) => {
+        expect(txt).toBe('createContract');
+      });
+    });
+  
+    it('createDeal component should be loadable',() => {
+      page.navigateTo('/createDeal');
+      browser.findElement(by.id('transactionName'))
+      .then((transactionName) => {
+        return transactionName.getText();
+      })
+      .then((txt) => {
+        expect(txt).toBe('createDeal');
+      });
+    });
+  
+    it('createSO component should be loadable',() => {
+      page.navigateTo('/createSO');
+      browser.findElement(by.id('transactionName'))
+      .then((transactionName) => {
+        return transactionName.getText();
+      })
+      .then((txt) => {
+        expect(txt).toBe('createSO');
+      });
+    });
+  
+    it('updateSOStatus component should be loadable',() => {
+      page.navigateTo('/updateSOStatus');
+      browser.findElement(by.id('transactionName'))
+      .then((transactionName) => {
+        return transactionName.getText();
+      })
+      .then((txt) => {
+        expect(txt).toBe('updateSOStatus');
+      });
+    });
+  
+    it('createInvoice component should be loadable',() => {
+      page.navigateTo('/createInvoice');
+      browser.findElement(by.id('transactionName'))
+      .then((transactionName) => {
+        return transactionName.getText();
+      })
+      .then((txt) => {
+        expect(txt).toBe('createInvoice');
+      });
+    });
+  
+
+});
